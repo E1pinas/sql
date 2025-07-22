06f8c1e22524947323dba4190d906364f33f4f5e
E1pinas <wadeider7x7@gmail.com>
2025-07-22 17:59:05 +0200

    Segundo video 16

--- clases.sql
+++ clases.sql
@@ -1,75 +1,253 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         11.8.2-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             12.10.0.7000
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Volcando estructura de base de datos para bd_aprendizaje
-CREATE DATABASE IF NOT EXISTS `bd_aprendizaje` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
-USE `bd_aprendizaje`;
-
--- Volcando estructura para tabla bd_aprendizaje.tb_articulos
-CREATE TABLE IF NOT EXISTS `tb_articulos` (
-  `codigo_articulos` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion_articulos` varchar(80) DEFAULT NULL,
-  `marca_articulo` varchar(30) DEFAULT NULL,
-  `codigo_unidad` int(11) DEFAULT NULL,
-  `codigo_categoria` int(11) DEFAULT NULL,
-  PRIMARY KEY (`codigo_articulos`),
-  KEY `FK_tb_articulos_tb_unidad` (`codigo_unidad`),
-  KEY `FK_tb_articulos_tb_categorias` (`codigo_categoria`),
-  CONSTRAINT `FK_tb_articulos_tb_categorias` FOREIGN KEY (`codigo_categoria`) REFERENCES `tb_categorias` (`codigo_categoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_tb_articulos_tb_unidad` FOREIGN KEY (`codigo_unidad`) REFERENCES `tb_unidad` (`codigo_unidad`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
--- Volcando datos para la tabla bd_aprendizaje.tb_articulos: ~2 rows (aproximadamente)
-INSERT INTO `tb_articulos` (`codigo_articulos`, `descripcion_articulos`, `marca_articulo`, `codigo_unidad`, `codigo_categoria`) VALUES
-	(1, 'ESCRITORIO', 'STANDARD', 1, 1),
-	(2, 'SILLA', 'STANDARD', 1, 2);
-
--- Volcando estructura para tabla bd_aprendizaje.tb_categorias
-CREATE TABLE IF NOT EXISTS `tb_categorias` (
-  `codigo_categoria` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion_categoria` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`codigo_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
--- Volcando datos para la tabla bd_aprendizaje.tb_categorias: ~4 rows (aproximadamente)
-INSERT INTO `tb_categorias` (`codigo_categoria`, `descripcion_categoria`) VALUES
-	(1, 'OFICINAS'),
-	(2, 'HOGAR'),
-	(3, 'FERRETERIA'),
-	(4, 'NEGOCIOS');
-
--- Volcando estructura para tabla bd_aprendizaje.tb_unidad
-CREATE TABLE IF NOT EXISTS `tb_unidad` (
-  `codigo_unidad` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion_unidad` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`codigo_unidad`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
--- Volcando datos para la tabla bd_aprendizaje.tb_unidad: ~6 rows (aproximadamente)
-INSERT INTO `tb_unidad` (`codigo_unidad`, `descripcion_unidad`) VALUES
-	(1, 'UNIDADES'),
-	(2, 'KILOGRAMOS'),
-	(3, 'LITROS'),
-	(4, 'METROS'),
-	(5, 'CAJAS'),
-	(6, 'PACKS');
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+SessionManager_WindowLeft<|||>3<|||>426
+SessionManager_WindowTop<|||>3<|||>180
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+MainWinOnMonitor<|||>3<|||>0
+MainWinWidth<|||>3<|||>683
+MainWinHeight<|||>3<|||>768
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,316
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,316,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,188
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,256
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+LastSessions<|||>1<|||>Cn_user_Sistema
+LastActiveSession<|||>1<|||>Cn_user_Sistema
+SessionManager_WindowHeight<|||>3<|||>488
+ThemePreviewWidth<|||>3<|||>1381
+ThemePreviewHeight<|||>3<|||>788
+ThemePreviewTop<|||>3<|||>0
+ThemePreviewLeft<|||>3<|||>-6
+SQL Attr Comment Foreground<|||>3<|||>8023636
+SQL Attr Comment Background<|||>3<|||>536870911
+SQL Attr Comment Style<|||>3<|||>2
+SQL Attr ConditionalComment Foreground<|||>3<|||>12108397
+SQL Attr ConditionalComment Background<|||>3<|||>536870911
+SQL Attr ConditionalComment Style<|||>3<|||>2
+SQL Attr ConsoleOutput Foreground<|||>3<|||>536870911
+SQL Attr ConsoleOutput Background<|||>3<|||>536870911
+SQL Attr ConsoleOutput Style<|||>3<|||>5
+SQL Attr DataType Foreground<|||>3<|||>15372999
+SQL Attr DataType Background<|||>3<|||>536870911
+SQL Attr DataType Style<|||>3<|||>1
+SQL Attr DefaultPackages Foreground<|||>3<|||>536870911
+SQL Attr DefaultPackages Background<|||>3<|||>536870911
+SQL Attr DefaultPackages Style<|||>3<|||>1
+SQL Attr DelimitedIdentifier Foreground<|||>3<|||>16757122
+SQL Attr DelimitedIdentifier Background<|||>3<|||>536870911
+SQL Attr DelimitedIdentifier Style<|||>3<|||>0
+SQL Attr Exception Foreground<|||>3<|||>536870911
+SQL Attr Exception Background<|||>3<|||>536870911
+SQL Attr Exception Style<|||>3<|||>2
+SQL Attr Function Foreground<|||>3<|||>14929603
+SQL Attr Function Background<|||>3<|||>536870911
+SQL Attr Function Style<|||>3<|||>1
+SQL Attr Identifier Foreground<|||>3<|||>16757122
+SQL Attr Identifier Background<|||>3<|||>536870911
+SQL Attr Identifier Style<|||>3<|||>0
+SQL Attr Key Foreground<|||>3<|||>14929603
+SQL Attr Key Background<|||>3<|||>536870911
+SQL Attr Key Style<|||>3<|||>1
+SQL Attr Number Foreground<|||>3<|||>7361535
+SQL Attr Number Background<|||>3<|||>536870911
+SQL Attr Number Style<|||>3<|||>0
+SQL Attr PLSQL-ReservedWord Foreground<|||>3<|||>536870911
+SQL Attr PLSQL-ReservedWord Background<|||>3<|||>536870911
+SQL Attr PLSQL-ReservedWord Style<|||>3<|||>1
+SQL Attr ProcName Foreground<|||>3<|||>536870911
+SQL Attr ProcName Background<|||>3<|||>536870911
+SQL Attr ProcName Style<|||>3<|||>0
+SQL Attr Space Foreground<|||>3<|||>536870911
+SQL Attr Space Background<|||>3<|||>536870911
+SQL Attr Space Style<|||>3<|||>0
+SQL Attr SQLPlus-Command Foreground<|||>3<|||>536870911
+SQL Attr SQLPlus-Command Background<|||>3<|||>536870911
+SQL Attr SQLPlus-Command Style<|||>3<|||>1
+SQL Attr String Foreground<|||>3<|||>8906947
+SQL Attr String Background<|||>3<|||>536870911
+SQL Attr String Style<|||>3<|||>0
+SQL Attr Symbol Foreground<|||>3<|||>12897152
+SQL Attr Symbol Background<|||>3<|||>536870911
+SQL Attr Symbol Style<|||>3<|||>0
+SQL Attr TableName Foreground<|||>3<|||>6911735
+SQL Attr TableName Background<|||>3<|||>536870911
+SQL Attr TableName Style<|||>3<|||>0
+SQL Attr Variable Foreground<|||>3<|||>7064575
+SQL Attr Variable Background<|||>3<|||>536870911
+SQL Attr Variable Style<|||>3<|||>0
+SQLColMatchingBraceForeground<|||>1<|||>$0028EFFF
+SQLColMatchingBraceBackground<|||>1<|||>$004D513B
+Theme<|||>1<|||>Glow
+FieldColor_Numeric<|||>3<|||>16750469
+FieldColor_Real<|||>3<|||>13663613
+FieldColor_Text<|||>3<|||>7591283
+FieldColor_Binary<|||>3<|||>13203071
+FieldColor_Datetime<|||>3<|||>7566281
+FieldColor_Spatial<|||>3<|||>13553267
+FieldColor_Other<|||>3<|||>7586241
+FieldEditor_Set<|||>3<|||>1
+MainWinLeft<|||>3<|||>0
+MainWinTop<|||>3<|||>0
+GroupTreeObjects<|||>3<|||>1
+sqloutheight<|||>3<|||>182
+MainWinMaximized<|||>3<|||>1
+ColWidths_UserManagerForm.listUsers<|||>1<|||>96,80
+ColsVisible_UserManagerForm.listUsers<|||>1<|||>0,1
+ColPositions_UserManagerForm.listUsers<|||>1<|||>0,1
+ColSort_UserManagerForm.listUsers<|||>1<|||>0,0
+CreateDbCollation<|||>1<|||>utf8mb4_general_ci
+FontSize<|||>3<|||>12
+ColWidths_frmTableEditor.listColumns<|||>1<|||>56,100,156,90,60,65,50,119,130,140,100,100,50,50
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>231,100,80,120,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>196,80,100,80,80,81
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,417
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+DataFontSize<|||>3<|||>10
+MemoEditorWidth<|||>3<|||>501
+MemoEditorHeight<|||>3<|||>201
+DateTimeEditor_CursorPos_Type4<|||>3<|||>10
+querymemoheight<|||>3<|||>134
+dbtreewidth<|||>3<|||>327
+queryhelperswidth<|||>3<|||>20
+SQLFile0<|||>1<|||>C:\Users\wadei\Downloads\clases.sql
+SQLFile1<|||>1<|||>C:\Users\wadei\AppData\Roaming\HeidiSQL\Backups\query-tab-2025-07-13_12-31-00-293.sql
+SQLFile2<|||>1<|||>C:\Users\wadei\Documents\oto\maria\archivo.sql
+SQLFile3<|||>1<|||>C:\Users\wadei\AppData\Roaming\HeidiSQL\Backups\query-tab-.sql
+FileDialogEncoding_MainForm<|||>3<|||>0
+TabCloseOnDoubleClick<|||>3<|||>1
+SQLHelp_WindowLeft<|||>3<|||>-8
+SQLHelp_WindowTop<|||>3<|||>-8
+SQLHelp_WindowWidth<|||>3<|||>1382
+SQLHelp_WindowHeight<|||>3<|||>784
+SQLHelp_PnlRightTopHeight<|||>3<|||>528
+Servers\CN_local\SessionCreated<|||>1<|||>2025-03-09 12:24:19
+Servers\CN_local\Host<|||>1<|||>localhost
+Servers\CN_local\WindowsAuth<|||>3<|||>0
+Servers\CN_local\CleartextPluginEnabled<|||>3<|||>0
+Servers\CN_local\User<|||>1<|||>root
+Servers\CN_local\Password<|||>1<|||>6C7370656F7865703637354
+Servers\CN_local\LoginPrompt<|||>3<|||>0
+Servers\CN_local\Port<|||>1<|||>3306
+Servers\CN_local\NetType<|||>3<|||>0
+Servers\CN_local\Compressed<|||>3<|||>0
+Servers\CN_local\LocalTimeZone<|||>3<|||>0
+Servers\CN_local\QueryTimeout<|||>3<|||>30
+Servers\CN_local\KeepAlive<|||>3<|||>20
+Servers\CN_local\FullTableStatus<|||>3<|||>1
+Servers\CN_local\Databases<|||>1<|||>
+Servers\CN_local\Library<|||>1<|||>libmariadb.dll
+Servers\CN_local\Comment<|||>1<|||>
+Servers\CN_local\StartupScriptFilename<|||>1<|||>
+Servers\CN_local\TreeBackground<|||>3<|||>536870911
+Servers\CN_local\SSHtunnelActive<|||>3<|||>0
+Servers\CN_local\SshExecutable<|||>1<|||>
+Servers\CN_local\SSHtunnelHost<|||>1<|||>
+Servers\CN_local\SSHtunnelHostPort<|||>3<|||>22
+Servers\CN_local\SSHtunnelUser<|||>1<|||>
+Servers\CN_local\SSHtunnelPassword<|||>1<|||>7
+Servers\CN_local\SSHtunnelTimeout<|||>3<|||>4
+Servers\CN_local\SSHtunnelPrivateKey<|||>1<|||>
+Servers\CN_local\SSHtunnelPort<|||>3<|||>3307
+Servers\CN_local\SSL_Active<|||>3<|||>0
+Servers\CN_local\SSL_Key<|||>1<|||>
+Servers\CN_local\SSL_Cert<|||>1<|||>
+Servers\CN_local\SSL_CA<|||>1<|||>
+Servers\CN_local\SSL_Cipher<|||>1<|||>
+Servers\CN_local\SSL_Verification<|||>3<|||>2
+Servers\CN_local\IgnoreDatabasePattern<|||>1<|||>
+Servers\CN_local\LogFileDdl<|||>3<|||>0
+Servers\CN_local\LogFileDml<|||>3<|||>0
+Servers\CN_local\LogFilePath<|||>1<|||>C:\Users\wadei\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\CN_local\ServerVersionFull<|||>1<|||>10.11.13-MariaDB - mariadb.org binary distribution
+Servers\CN_local\ConnectCount<|||>3<|||>5
+Servers\CN_local\ServerVersion<|||>3<|||>101113
+Servers\CN_local\LastConnect<|||>1<|||>2025-07-13 12:28:36
+Servers\CN_local\lastUsedDB<|||>1<|||>ot_db
+Servers\CN_local\RefusedCount<|||>3<|||>8
+Servers\CN_local\QueryHistory\1<|||>1<|||>2025-07-13 12:31:23|ot_bd|32|-- Crear la base de datos<{{{><}}}>CREATE DATABASE IF NOT EXISTS ot_db;<{{{><}}}><{{{><}}}>-- Usar esa base<{{{><}}}>USE ot_db;<{{{><}}}><{{{><}}}>-- Crear la tabla usuarios<{{{><}}}>CREATE TABLE usuarios (<{{{><}}}>  id INT AUTO_INCREMENT PRIMARY KEY,<{{{><}}}>  nombre VARCHAR(100) NOT NULL,<{{{><}}}>  correo VARCHAR(100) UNIQUE NOT NULL,<{{{><}}}>  edad INT,<{{{><}}}>  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP<{{{><}}}>);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\CN_local\QueryHistory\2<|||>1<|||>2025-07-13 12:33:27|ot_bd|32|SELECT *FROM usuarios;
+Servers\CN_local\QueryHistory\3<|||>1<|||>2025-07-13 12:33:41|ot_bd|15|INSERT INTO usuarios (nombre, correo, edad) VALUES<{{{><}}}>('Scarlett Edevane', 'scarlett@example.com', 21),<{{{><}}}>('Dante Sorel', 'dante@example.com', 22),<{{{><}}}>('Noah Elenvar', 'noah@example.com', 20);
+Servers\CN_local\QueryHistory\4<|||>1<|||>2025-07-13 12:34:10|ot_bd|0|<{{{><}}}><{{{><}}}>SELECT * FROM usuarios;
+Servers\Cn_user_Sistema\SessionCreated<|||>1<|||>2025-03-09 12:50:59
+Servers\Cn_user_Sistema\Host<|||>1<|||>localhost
+Servers\Cn_user_Sistema\WindowsAuth<|||>3<|||>0
+Servers\Cn_user_Sistema\CleartextPluginEnabled<|||>3<|||>0
+Servers\Cn_user_Sistema\User<|||>1<|||>root
+Servers\Cn_user_Sistema\Password<|||>1<|||>6F767368727B687338393A7
+Servers\Cn_user_Sistema\LoginPrompt<|||>3<|||>0
+Servers\Cn_user_Sistema\Port<|||>1<|||>3306
+Servers\Cn_user_Sistema\NetType<|||>3<|||>0
+Servers\Cn_user_Sistema\Compressed<|||>3<|||>0
+Servers\Cn_user_Sistema\LocalTimeZone<|||>3<|||>0
+Servers\Cn_user_Sistema\QueryTimeout<|||>3<|||>30
+Servers\Cn_user_Sistema\KeepAlive<|||>3<|||>20
+Servers\Cn_user_Sistema\FullTableStatus<|||>3<|||>1
+Servers\Cn_user_Sistema\Databases<|||>1<|||>
+Servers\Cn_user_Sistema\Library<|||>1<|||>libmariadb.dll
+Servers\Cn_user_Sistema\Comment<|||>1<|||>
+Servers\Cn_user_Sistema\StartupScriptFilename<|||>1<|||>
+Servers\Cn_user_Sistema\TreeBackground<|||>3<|||>536870911
+Servers\Cn_user_Sistema\SSHtunnelActive<|||>3<|||>0
+Servers\Cn_user_Sistema\SshExecutable<|||>1<|||>plink.exe
+Servers\Cn_user_Sistema\SSHtunnelHost<|||>1<|||>
+Servers\Cn_user_Sistema\SSHtunnelHostPort<|||>3<|||>22
+Servers\Cn_user_Sistema\SSHtunnelUser<|||>1<|||>
+Servers\Cn_user_Sistema\SSHtunnelPassword<|||>1<|||>2
+Servers\Cn_user_Sistema\SSHtunnelTimeout<|||>3<|||>4
+Servers\Cn_user_Sistema\SSHtunnelPrivateKey<|||>1<|||>
+Servers\Cn_user_Sistema\SSHtunnelPort<|||>3<|||>3307
+Servers\Cn_user_Sistema\SSL_Active<|||>3<|||>0
+Servers\Cn_user_Sistema\SSL_Key<|||>1<|||>
+Servers\Cn_user_Sistema\SSL_Cert<|||>1<|||>
+Servers\Cn_user_Sistema\SSL_CA<|||>1<|||>
+Servers\Cn_user_Sistema\SSL_Cipher<|||>1<|||>
+Servers\Cn_user_Sistema\SSL_Verification<|||>3<|||>2
+Servers\Cn_user_Sistema\IgnoreDatabasePattern<|||>1<|||>
+Servers\Cn_user_Sistema\LogFileDdl<|||>3<|||>0
+Servers\Cn_user_Sistema\LogFileDml<|||>3<|||>0
+Servers\Cn_user_Sistema\LogFilePath<|||>1<|||>C:\Users\wadei\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\Cn_user_Sistema\RefusedCount<|||>3<|||>5
+Servers\Cn_user_Sistema\ServerVersionFull<|||>1<|||>10.11.13-MariaDB - mariadb.org binary distribution
+Servers\Cn_user_Sistema\ConnectCount<|||>3<|||>7
+Servers\Cn_user_Sistema\ServerVersion<|||>3<|||>101113
+Servers\Cn_user_Sistema\LastConnect<|||>1<|||>2025-07-22 16:58:41
+Servers\Cn_user_Sistema\lastUsedDB<|||>1<|||>bd_aprendizaje
+Servers\Cn_user_Sistema\FavoriteObjects<|||>1<|||>
+Servers\Cn_user_Sistema\bd_aprendizaje|tb_unidades_medidas\Sort<|||>1<|||>1_codigo_um|
+Servers\Cn_user_Sistema\QueryHistory\25<|||>1<|||>2025-07-22 16:59:38|bd_aprendizaje|16|-- Crear la base de datos<{{{><}}}>CREATE DATABASE IF NOT EXISTS ot_db;<{{{><}}}><{{{><}}}>-- Usar esa base<{{{><}}}>USE ot_db;<{{{><}}}><{{{><}}}>-- Crear la tabla usuarios<{{{><}}}>CREATE TABLE usuarios (<{{{><}}}>  id INT AUTO_INCREMENT PRIMARY KEY,<{{{><}}}>  nombre VARCHAR(100) NOT NULL,<{{{><}}}>  correo VARCHAR(100) UNIQUE NOT NULL,<{{{><}}}>  edad INT,<{{{><}}}>  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP<{{{><}}}>);<{{{><}}}><{{{><}}}>INSERT INTO usuarios (nombre, correo, edad) VALUES<{{{><}}}>('Scarlett Edevane', 'scarlett@example.com', 21),<{{{><}}}>('Dante Sorel', 'dante@example.com', 22),<{{{><}}}>('Noah Elenvar', 'noah@example.com', 20);<{{{><}}}><{{{><}}}>
+Servers\Cn_user_Sistema\QueryHistory\1<|||>1<|||>2025-07-22 17:21:53|bd_aprendizaje|0|SELECT * FROM tb_articulos;
+Servers\Cn_user_Sistema\QueryHistory\2<|||>1<|||>2025-07-22 17:33:43|bd_aprendizaje|0|UPDATE tb_categorias SET descripcion_categoria='CENTRO COMERCIAL'<{{{><}}}>WHERE codigo_categoria=5;
+Servers\Cn_user_Sistema\QueryHistory\3<|||>1<|||>2025-07-22 17:37:44|bd_aprendizaje|0|UPDATE tb_categorias SET descripcion_categoria =CONCAT(descripcion_categoria,' *')<{{{><}}}>WHERE codigo_categoria IN (1,2,3);
+Servers\Cn_user_Sistema\QueryHistory\4<|||>1<|||>2025-07-22 17:55:42|bd_aprendizaje|0|SELECT * FROM tb_categorias WHERE codigo_categoria =2;
+Servers\Cn_user_Sistema\QueryHistory\5<|||>1<|||>2025-07-22 17:56:18|bd_aprendizaje|0|SELECT * FROM tb_articulos WHERE codigo_categoria =2;
